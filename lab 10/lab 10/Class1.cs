7beb371aeff32813586799974e15fe72e0afcb03
SerzhikY <124130138+SerzhikY@users.noreply.github.com>
2023-04-10 22:48:17 +0300

    Add files via upload

---
+++ lab 10/lab 10/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_10
+{
+    public interface IResource
+    {
+        void AccessResource();
+    }
+
+    
+    public class ConcreteResource : IResource
+    {
+        public void AccessResource()
+        {
+            MessageBox.Show("Correct");
+        }
+    }
+
+   
+    public class ResourceProxy : IResource
+    {
+        private ConcreteResource resource;
+        private string username;
+        private string password;
+
+        public ResourceProxy(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public void AccessResource()
+        {
+            if (Authenticate())
+            {
+                resource = new ConcreteResource();
+                resource.AccessResource();
+            }
+            else
+            {
+                MessageBox.Show("Wrong");
+            }
+        }
+
+        private bool Authenticate()
+        {
+            return username == "1" && password == "1";
+
+        }
+    }
+}
